eb187d278f7be102a83d560a6daf3a73f370f33e
luis martinez <s_k_ap3@hotmail.com>
2020-09-29 16:59:59 -0500

    correccion de sintaxis en el archivo .php

--- upload-manager.php
+++ upload-manager.php
@@ -1,38 +1,38 @@
 <?php
-// Check if the form was submitted
+// verfifica que el servidor permita subir archivos
 //if($_SERVER["REQUEST_METHOD"] == "POST"){
-    // Check if file was uploaded without errors
+    // Verifica errores en los archivos a subir
     if(isset($_FILES["photo"]) && $_FILES["photo"]["error"] == 0){
         $allowed = array("jpg" => "image/jpg", "jpeg" => "image/jpeg", "gif" => "image/gif", "png" => "image/png");
         $filename = $_FILES["photo"]["name"];
         $filetype = $_FILES["photo"]["type"];
         $filesize = $_FILES["photo"]["size"];
     
-        // Verify file extension
+        // verfica la extencion
         $ext = pathinfo($filename, PATHINFO_EXTENSION);
-        if(!array_key_exists($ext, $allowed)) die("Error: Please select a valid file format.");
+        if(!array_key_exists($ext, $allowed)) die("Error: Por favor seleccione un formato valido");
     
-        // Verify file size - 5MB maximum
+        // verifica tama帽o maximo - 5MB maximum
         $maxsize = 5 * 1024 * 1024;
-        if($filesize > $maxsize) die("Error: File size is larger than the allowed limit.");
+        if($filesize > $maxsize) die("Error: el archivo es demaciado grande");
     
         // Verify MYME type of the file
         if(in_array($filetype, $allowed)){
             // Check whether file exists before uploading it
             if(file_exists("upload/" . $filename)){
-                echo $filename . " is already exists.";
+                echo $filename . "el archivo ya existe";
             } else{
                 //move_uploaded_file($_FILES["photo"]["tmp_name"], "upload/" . $filename);
-                echo "Your file was uploaded successfully.";
+                echo "su archivo se cargo correctamente";
             } 
         } else{
-            echo "Error: There was a problem uploading your file. Please try again."; 
+            echo "Error: Ocurrio un error durante la carga por favor intente de nuevo"; 
         }
     } else{
         echo "Error: " . $_FILES["photo"]["error"];
     }
 /*}
 else{
-    echo "el server no permite post"
+    echo "el server no permite post";
 }*/
 ?>
